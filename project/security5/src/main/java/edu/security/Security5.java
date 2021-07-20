0f351ea9a2f53d7b471946b5d4ac8e003f8cc4d2
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2021-07-21 01:27:16 +0300

    authorization spring security

---
+++ project/security5/src/main/java/edu/security/Security5.java
@@ -0,0 +1,13 @@
+package edu.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security5 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security5.class, args);
+	}
+
+}
---
+++ project/security5/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table my_user(
+ login VARCHAR(50) not null primary key,
+ password VARCHAR(50) not null,
+ position  VARCHAR(50) not null,
+ authority VARCHAR(50) not null
+ );
